06c6fcf4da45775f3a93a9b26230b38fa4f0d7f5
EverettAM <167331153+EverettAM@users.noreply.github.com>
2024-04-19 11:37:57 -0500

    Update ItemsToPurchase.cpp

--- ItemsToPurchase.cpp
+++ ItemsToPurchase.cpp
@@ -3,9 +3,30 @@ using namespace std;
 
 #include "ItemToPurchase.h"
 
-void ItemToPurchase::SetName(string newName){
+void ItemsToPurchase::SetName(string newName){
   itemName = newName;
 }
-string ItemToPurchase::GetName(){
+string ItemsToPurchase::GetName(){
   return name;
+}
+
+void ItemsToPurchase::SetPrice(int newPrice){
+  itemPrice = newPrice;
+}
+int ItemsToPurchase::GetPrice(){
+  return itemPrice;
+}
+
+void ItemsToPurchase::SetQuantity(int newQuantity){
+  itemQuantity = newQuantity;
+}
+
+int ItemsToPurchase::GetQuantity(){
+  return itemQuantity;
+}
+
+ItemsToPurchase::ItemsToPurchase(){
+  itemName = "none";
+  itemPrice = 0;
+  itemQuantity = 0;
 }
